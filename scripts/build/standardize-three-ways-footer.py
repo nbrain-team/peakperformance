c0c97173475fac8e9464e3f3d960725e736d76fc
Bill Douglas <bill@billdouglas.local>
2026-05-13 07:55:39 -0600

    auto: file changes 2026-05-13 07:55:39

--- scripts/build/standardize-three-ways-footer.py
+++ scripts/build/standardize-three-ways-footer.py
@@ -86,7 +86,6 @@ NEW_CTA_ROWS_TUPLE = (
     '[\\"$\\",\\"div\\",null,{\\"className\\":\\"three-ways-blocks__cta-row three-ways-blocks__cta-row--secondary\\",\\"children\\":['
     '[\\"$\\",\\"$L5\\",\\"ppp-cta-review\\",{\\"href\\":\\"/ppp-review\\",\\"className\\":\\"btn btn-lg three-ways-blocks__cta-review\\",\\"children\\":\\"Request PPP Review\\"}]'
     ']}]'
-    ']}]'
 )
 
 
